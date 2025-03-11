7f7d30d47f13b80a688fd07ec1d8eab397fc6356
koh-johanhostens <71190734+koh-johanhostens@users.noreply.github.com>
2025-03-11 11:13:27 +0100

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,6 +1,5 @@
 # Import python packages
 import streamlit as st
-from snowflake.snowpark.context import get_active_session
 from snowflake.snowpark.functions import col
 
 helpful_links = [
@@ -20,7 +19,8 @@ name_on_order = st.text_input('Name of Smoothie:')
 st.write('The name of your smoothie will be', name_on_order)
 
 # Get the current credentials
-session = get_active_session()
+cnx = st.connection("snowflake")
+session = cnx.session()
 my_dataframe = session.table("smoothies.public.fruit_options").select(col('FRUIT_NAME'))
 # st.dataframe(data=my_dataframe, use_container_width=True)
 
